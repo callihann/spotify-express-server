3519cd6c4e5c1cb819998464b43c9a889b261bf1
Marlowe <moliver25@seaburyhall.org>
2022-11-23 17:30:27 -1000

    fixed position slider and added colorthief dependecy

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
+var colorThief = new ColorThief();
 var pos_slider = document.getElementById("position");   
 var slider = document.getElementById("volRange");
+const img = new Image();
 
 function getCookie(name) { // https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
         var nameEQ = name + "=";
@@ -20,13 +22,22 @@ window.onSpotifyWebPlaybackSDKReady = () => {
                 volume: 0.5
 });
 
+function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+}
+      
+function rgbToHex(r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+      
 var currState = {}
 function update() {
         player.getCurrentState().then(state => {
-        if (!state) {
-                console.error('User is not playing music through the Web Playback SDK');
-                return;
-        }
+                if (!state) {
+                        console.error('User is not playing music through the Web Playback SDK');
+                        return;
+                }
                 currState.paused = state.paused;
                 currState.position = state.position;
                 currState.duration = state.duration;
@@ -34,6 +45,15 @@ function update() {
                 document.getElementById("trackname").innerHTML = state.track_window.current_track['name'];
                 document.getElementById("artistname").innerHTML = state.track_window.current_track['artists'][0]['name'];
                 document.getElementById("image").src=state.track_window.current_track['album']['images'][0]['url'];
+                img.addEventListener('load', function() {
+                        const out = colorThief.getColor(img);
+                        console.log(typeof Object.values(out));
+                        for (const i in Object(out)) {
+                                console.log(i);
+                        };
+                });
+                img.crossOrigin = 'Anonymous'
+                img.src = state.track_window.current_track['album']['images'][0]['url']                 
         });
 }
 
@@ -42,7 +62,6 @@ function getStatePosition() { // https://github.com/spotify/web-playback-sdk/iss
                 return currState.position;
         }
         let position = currState.position + (performance.now() - currState.updateTime) / 1000;
-        // console.log("position: " + position + " " + "performance.now: " + currState.updateTime + " " + "currState.position: " + currState.position)
         return position > currState.duration ? currState.duration : position;
 }
 
@@ -61,6 +80,7 @@ pos_slider.oninput = function() {
 setInterval(function() { updatePosition() }, 1);
 function updatePosition() {
         const pos_val = getStatePosition()
+        // console.log((pos_val / currState.duration) * 100000) logging command for debug
         document.getElementById("position").value = ((pos_val / currState.duration) * 100000);
 }
 
@@ -100,6 +120,18 @@ document.getElementById('back').onclick = function() {
 };
 
 player.addListener('player_state_changed', update);
+
+document.addEventListener("keypress", function onEvent(event) {
+        if (event.key == "D") {
+                player.nextTrack();
+        }
+        else if (event.key == "A") {
+                player.previousTrack();
+        }
+        else if (event.key == "S") {
+                player.togglePlay();
+        }
+});
 
 player.connect();
 }
